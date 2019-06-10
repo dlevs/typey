03b552a057b10fa9f6c74449ead82f19577055b2
Daniel Levett <dlevett@hotmail.co.uk>
2019-06-10 22:31:39 +0100

    Add user input and basic display

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const TextDisplay = ({
+  value,
+  targetValue
+}: {
+  value: string
+  targetValue: string
+}) => (
+  <div
+    className="textDisplay"
+  >
+    {[...targetValue].map((char, i) => {
+      const className = value[i] === undefined
+        ? 'charInactive'
+        : char === value[i]
+          ? 'charSuccess'
+          : 'charError'
+
+      return (
+        <span
+          className={`char ${className}`}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {char}
+        </span>
+      )
+    })}
+  </div>
+)
+
+const TextInput = ({
+  value,
+  onChange
+}: React.HTMLProps<HTMLTextAreaElement>) => (
+  <textarea
+    className="textInput"
+    value={value}
+    onChange={onChange}
+  />
+)
+
+const App: React.FC = () => {
+  const [textInput, setTextInput] = useState('')
+  return (
+    <>
+      <TextDisplay
+        value={textInput}
+        targetValue="The fight isn't over until you win it, Fitz. That's all you have to remember. No matter what the other man says."
+      />
+      <TextInput
+        value={textInput}
+        onChange={e => setTextInput(e.currentTarget.value)}
+      />
+    </>
   );
 }
 
